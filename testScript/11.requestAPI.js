ed4cfe345e3baf2f8b25ddef7a13f957a470549f
mahiroaug <nrs020013@gmail.com>
2024-05-19 13:26:53 +0000

    01.createVandM

--- testScript/11.requestAPI.js
+++ testScript/11.requestAPI.js
@@ -1,83 +1,219 @@
 require("dotenv").config({ path: ".env" });
 
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const { Web3 } = require("web3");
+const {
+  FireblocksSDK,
+  PeerType,
+  TransactionOperation,
+  TransactionStatus,
+} = require("fireblocks-sdk");
+const {
+  FireblocksWeb3Provider,
+  ChainId,
+} = require("@fireblocks/fireblocks-web3-provider");
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 
-// environment variables------------------------------------------
-const axios = require("axios");
+// -------------------COMMON----------------------- //
+//// GSN
 const apiKey = process.env.API_GATEWAY_APIKEY;
-const API_GATEWAY_PATH = process.env.API_GATEWAY_PATH;
-const apiUrl = `https://${API_GATEWAY_PATH}.execute-api.ap-northeast-1.amazonaws.com/v1/raw/token/ERC2612Permit`;
+const apiUrl = process.env.API_GATEWAY_URL;
+const apiUrl_registVault = `${apiUrl}/vaults/bulkInsert`;
+const apiUrl_ERC2771 = `${apiUrl}/raw/token/ERC2771`;
+const apiUrl_ERC2612Permit = `${apiUrl}/raw/token/ERC2612Permit`;
+const DOMAIN_NAME = process.env.DOMAIN_SEPARATOR_PARAM_NAME_TOKEN;
+const DOMAIN_VERSION = process.env.DOMAIN_SEPARATOR_PARAM_VERSION_TOKEN;
+const DOMAIN_VERIFYINGCONTRACT = process.env.TOKENPROXY_CA;
 
-// include vaults file
+///// vaults
 const vaultsRaw = require("../.env.vaults");
-const vaults = vaultsRaw.vaults;
+const minters = vaultsRaw.minters;
 
+//// token
 const TOKEN_CA = process.env.TOKENPROXY_CA;
 const TOKEN_ABI =
   require("../artifacts/contracts/V21/JST_V21.sol/JST_V21.json").abi;
+
+//// explorer
+const EXPLOERE = process.env.EXPLOERE;
+
+//// fireblocks
+const chainId = ChainId.POLYGON_AMOY; // Polygon Testnet(amoy)
+const rpcUrl = process.env.POLYGON_RPC_URL;
+const BASE_ASSET_ID = process.env.FIREBLOCKS_ASSET_ID;
+const TOKEN_ASSET_ID = process.env.FIREBLOCKS_ASSET_ID_MYTOKEN;
+
+// -------------------FIREBLOCKS------------------- //
+//// fireblocks - SDK
+const fb_apiSecret = fs.readFileSync(
+  path.resolve("fireblocks_secret_SIGNER.key"),
+  "utf8"
+);
+const fb_apiKey = process.env.FIREBLOCKS_API_KEY_SIGNER;
+const fb_base_url = process.env.FIREBLOCKS_URL;
+const fireblocks = new FireblocksSDK(fb_apiSecret, fb_apiKey, fb_base_url);
 
 //// alchemy
 const alchemyHTTPS = process.env.ALCHEMY_HTTPS;
 const web3_alchemy = createAlchemyWeb3(alchemyHTTPS);
 const token = new web3_alchemy.eth.Contract(TOKEN_ABI, TOKEN_CA);
 
-// transfer-----------------------------------------------------
-async function sendRequest_raw() {
-  // payload
-  const DOMAIN_SEPARATOR_PARAM_NAME_TOKEN =
-    process.env.DOMAIN_SEPARATOR_PARAM_NAME_TOKEN;
-  const DOMAIN_SEPARATOR_PARAM_VERSION_TOKEN =
-    process.env.DOMAIN_SEPARATOR_PARAM_VERSION_TOKEN;
-  const DOMAIN_VERIFYINGCONTRACT = process.env.TOKENPROXY_CA;
-  const owner_addr = vaults[0].address;
-  const spender_addr = vaults[1].address;
+/////////////////////////////////////////
+////// tool function ///////////////////
+/////////////////////////////////////////
 
-  const amount = 1000;
-  const weiAmount = await web3_alchemy.utils.toWei(amount.toString(), "ether");
-  const value = weiAmount;
+async function _createVaultAccounts(assetId, vaultAccountNamePrefix) {
+  let vaultRes;
+  let vault;
+  let vaultWallet;
 
-  const nonceStr = await token.methods.nonces(owner_addr).call();
-  const nonce = parseInt(nonceStr);
-
-  const payload = {
-    DOMAIN_NAME: DOMAIN_SEPARATOR_PARAM_NAME_TOKEN,
-    DOMAIN_VERSION: DOMAIN_SEPARATOR_PARAM_VERSION_TOKEN,
-    DOMAIN_VERIFYINGCONTRACT: DOMAIN_VERIFYINGCONTRACT,
-    owner: owner_addr,
-    spender: spender_addr,
-    value: value,
-    nonce: nonce,
+  vaultRes = await fireblocks.createVaultAccount(
+    vaultAccountNamePrefix.toString()
+  );
+  vault = {
+    vaultName: vaultRes.name,
+    vaultID: vaultRes.id,
   };
-  console.log("payload:: ", payload);
+  vaultWallet = await fireblocks.createVaultAsset(
+    Number(vault.vaultID),
+    assetId
+  );
+  return { vault, vaultWallet };
+}
 
-  return;
+async function _createVaultAsset(vaultId, assetId) {
+  const Res = await fireblocks.createVaultAsset(vaultId, assetId);
+  return Res;
+}
+
+async function createVault(assetId, accountName, tokenId) {
+  const { vault, vaultWallet } = await _createVaultAccounts(
+    assetId,
+    accountName
+  );
+  await _createVaultAsset(vault.vaultID, tokenId);
+
+  console.log(
+    `{vaultName: ${accountName}, vaultID: ${vault.vaultID}, address: ${vaultWallet.address}},`
+  );
+
+  resVault = {
+    vaultId: vault.vaultID,
+    name: accountName,
+    address: vaultWallet.address,
+  };
+
+  return resVault;
+}
+
+async function bulkInsertVault(vault) {
+  const requestParam = {
+    vaultId: vault.vaultId,
+    name: vault.name,
+    address: vault.address,
+  };
+  console.log("bulkInsertVault:requestParam::", requestParam);
 
   try {
-    const response = await axios.post(apiUrl, requestData, {
+    const response = await axios.post(apiUrl_registVault, requestParam, {
       headers: {
         "Content-Type": "application/json",
         "x-api-key": apiKey,
       },
     });
     console.log(`Request:: Status Code: ${response.status}`);
+    //console.log("--response:", response);
+
+    return response;
   } catch (error) {
     console.error(`Request:: Error: ${error.message}`);
   }
 }
 
-// main function-------------------------------------------
+async function mintToken(target_addr, amount) {
+  // from address (minter address)
+  const source = target_addr;
+  const minerIndex = parseInt(source.slice(-1), 16) % 8;
+  const from_addr = minters[minerIndex].address;
+
+  // to address (token address)
+  const to_addr = TOKEN_CA;
+
+  // data
+  const weiAmount = web3_alchemy.utils.toWei(amount.toString(), "ether");
+  const data = token.methods.mint(target_addr, weiAmount);
+
+  // set request param
+  const requestParam = {
+    from_addr: from_addr,
+    to_addr: to_addr,
+    data: data.encodeABI(),
+  };
+
+  console.log("mintToken:requestParam::", requestParam);
+
+  try {
+    const response = await axios.post(apiUrl_ERC2771, requestParam, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": apiKey,
+      },
+    });
+    console.log(`Request:: Status Code: ${response.status}`);
+    //console.log("--response:", response);
+
+    return response;
+  } catch (error) {
+    console.error(`Request:: Error: ${error.message}`);
+  }
+}
+
+async function sleepForSeconds(amount) {
+  console.log(`Sleeping for ${amount} seconds...`);
+  await new Promise((r) => setTimeout(r, amount * 1000)); // milliseconds
+  console.log(`${amount} seconds have passed!`);
+}
+
+/////////////////////////////////////////
+////// public functions /////////////////
+/////////////////////////////////////////
+
+async function createVaultAndMint(cardId, name) {
+  // step 1-1A : create vault
+  console.log(
+    "step 1-1A : create vault--------------------------------------------"
+  );
+  const resVault = await createVault(BASE_ASSET_ID, name, TOKEN_ASSET_ID);
+  console.log("createVaultAndMint:resVault::", resVault);
+  await sleepForSeconds(4);
+
+  // step 1-1B : vaults bulk insert
+  console.log(
+    "step 1-1B : vaults bulk insert--------------------------------------"
+  );
+  const resInsert = await bulkInsertVault(resVault);
+  console.log("createVaultAndMint:resInsert::", resInsert.data);
+  await sleepForSeconds(4);
+
+  // step 1-1C : mint token
+  console.log(
+    "step 1-1C : mint token----------------------------------------------"
+  );
+  const resApi = await mintToken(resVault.address, 1000);
+  console.log("createVaultAndMint:resApi::", resApi.data);
+  await sleepForSeconds(2);
+}
+
+/////////////////////////////////////////
+////// main functions /////////////////
+/////////////////////////////////////////
+
 (async () => {
-  console.log("Number of vaults:", vaults.length);
+  await createVaultAndMint("id-123456789", "testName0008");
 
-  sendRequest_raw().then(() => {
-    console.log("requests sent.");
-  });
-
-  // シグナルハンドリングでプロセスを停止
-  process.on("SIGINT", () => {
-    console.log("Process terminated.");
-    process.exit(0);
-  });
+  console.log("Done!");
 })().catch((e) => {
   console.error(`Failed: ${e}`);
   exit(-1);
---
+++ tools/01.addMinters.js
@@ -0,0 +1,431 @@
+require("dotenv").config({ path: ".env" });
+
+const fs = require("fs");
+const path = require("path");
+const { Web3 } = require("web3");
+//const { inspect } = require('util');
+const {
+  FireblocksSDK,
+  PeerType,
+  TransactionOperation,
+  TransactionStatus,
+} = require("fireblocks-sdk");
+const {
+  FireblocksWeb3Provider,
+  ChainId,
+} = require("@fireblocks/fireblocks-web3-provider");
+//const { sign } = require('crypto');
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+
+// -------------------COMMON----------------------- //
+//// common environment
+
+const vaultsRaw = require("../.env.vaults");
+const { exit } = require("process");
+const minters = vaultsRaw.minters;
+
+const chainId = ChainId.POLYGON_AMOY; // Polygon Testnet(amoy)
+const rpcUrl = process.env.POLYGON_RPC_URL;
+const EXPLOERE = process.env.EXPLOERE;
+
+const TOKEN_CA = process.env.TOKENPROXY_CA;
+const TOKEN_ABI =
+  require("../artifacts/contracts/V21/JST_V21.sol/JST_V21.json").abi;
+const FORWARDER_CA = process.env.FORWARDER_CA;
+const FORWARDER_ABI =
+  require("../artifacts/contracts/V21/IForwarder_V21.sol/IForwarder.json").abi;
+const ASSETID = process.env.FIREBLOCKS_ASSET_ID;
+
+const EIP712_DOMAIN_TYPE =
+  "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+const GENERIC_PARAMS =
+  "address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data,uint256 validUntilTime";
+const REQUEST_TYPE = "ForwardRequest(" + GENERIC_PARAMS + ")";
+const DOMAIN_SEPARATOR_PARAM_NAME = process.env.DOMAIN_SEPARATOR_PARAM_NAME;
+const DOMAIN_SEPARATOR_PARAM_VERSION =
+  process.env.DOMAIN_SEPARATOR_PARAM_VERSION;
+
+// -------------------FIREBLOCKS------------------- //
+//// fireblocks - SDK
+const fb_apiSecret = fs.readFileSync(
+  path.resolve("fireblocks_secret_SIGNER.key"),
+  "utf8"
+);
+const fb_apiKey = process.env.FIREBLOCKS_API_KEY_SIGNER;
+const fb_base_url = process.env.FIREBLOCKS_URL;
+const fireblocks = new FireblocksSDK(fb_apiSecret, fb_apiKey, fb_base_url);
+
+//// fireblocks - web3 provider - signer account
+const fb_vaultId = process.env.FIREBLOCKS_VAULT_ACCOUNT_ID_CONTRACTOWNER;
+const eip1193Provider = new FireblocksWeb3Provider({
+  privateKey: fb_apiSecret,
+  apiKey: fb_apiKey,
+  vaultAccountIds: fb_vaultId,
+  chainId: chainId,
+  rpcUrl: rpcUrl,
+});
+const web3 = new Web3(eip1193Provider);
+const token = new web3.eth.Contract(TOKEN_ABI, TOKEN_CA);
+const forwarder = new web3.eth.Contract(FORWARDER_ABI, FORWARDER_CA);
+
+//// fireblocks - web3 provider - relayer account
+const fb_vaultId_relayer = process.env.FIREBLOCKS_VAULT_ACCOUNT_ID_RELAYER;
+const eip1193Provider_withRelayer = new FireblocksWeb3Provider({
+  privateKey: fb_apiSecret,
+  apiKey: fb_apiKey,
+  vaultAccountIds: fb_vaultId_relayer,
+  chainId: chainId,
+  rpcUrl: rpcUrl,
+});
+const web3_withRelayer = new Web3(eip1193Provider_withRelayer);
+const token_withRelayer = new web3_withRelayer.eth.Contract(
+  TOKEN_ABI,
+  TOKEN_CA
+);
+const forwarder_withRelayer = new web3_withRelayer.eth.Contract(
+  FORWARDER_ABI,
+  FORWARDER_CA
+);
+
+//// alchemy
+const alchemyHTTPS = process.env.ALCHEMY_HTTPS;
+const web3_alchemy = createAlchemyWeb3(alchemyHTTPS);
+
+/////////////////////////////////////////
+////// sign functions ///////////////////
+/////////////////////////////////////////
+
+async function signEIP712Message(vaultAccountId, signRequest) {
+  const { status, id } = await fireblocks.createTransaction({
+    operation: TransactionOperation.TYPED_MESSAGE,
+    assetId: ASSETID,
+    source: {
+      type: PeerType.VAULT_ACCOUNT,
+      id: vaultAccountId,
+    },
+    amount: "0",
+    note: "TYPED(EIP-712) Message",
+    extraParameters: {
+      rawMessageData: {
+        messages: [signRequest],
+      },
+    },
+  });
+
+  let currentStatus = status;
+  let txInfo = await fireblocks.getTransactionById(id);
+
+  while (
+    currentStatus != TransactionStatus.COMPLETED &&
+    currentStatus != TransactionStatus.FAILED
+  ) {
+    console.log("keep polling for tx " + id + "; status: " + currentStatus);
+    txInfo = await fireblocks.getTransactionById(id);
+    currentStatus = txInfo.status;
+    await new Promise((r) => setTimeout(r, 1000));
+  }
+  if (currentStatus == TransactionStatus.FAILED) {
+    throw "Transaction failed. Substatus: " + txInfo.subStatus;
+  }
+
+  console.log(">>> txinfo");
+  console.log("txid:", id);
+  console.log("txinfo: ", txInfo);
+
+  console.log(">>> result");
+  const walletAddresses = await fireblocks.getDepositAddresses(
+    vaultAccountId,
+    ASSETID
+  );
+  console.log("walletAddresses: ", walletAddresses);
+  console.log("Address: ", walletAddresses[0].address);
+  console.log("signRequest: ", signRequest);
+
+  const signature = txInfo.signedMessages[0].signature;
+  const v = 27 + signature.v;
+  console.log("Signature(original): ", signature);
+  console.log("Signature.v(+27):", v.toString(16));
+
+  const vHex = web3.utils.toHex(v).slice(2);
+  const sigHex = "0x" + signature.fullSig + vHex;
+  console.log("sigHex: ", sigHex);
+  const sigByte = web3.utils.hexToBytes(sigHex);
+  console.log("sigByte: ", sigByte);
+
+  return {
+    v: v,
+    r: "0x" + signature.r,
+    s: "0x" + signature.s,
+    sigHex: sigHex,
+    sigByte: sigByte,
+  };
+}
+
+async function createRequest(req) {
+  return (SignRequest = {
+    type: "EIP712",
+    index: 0,
+    content: {
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        ForwardRequest: [
+          { name: "from", type: "address" },
+          { name: "to", type: "address" },
+          { name: "value", type: "uint256" },
+          { name: "gas", type: "uint256" },
+          { name: "nonce", type: "uint256" },
+          { name: "data", type: "bytes" },
+          { name: "validUntilTime", type: "uint256" },
+        ],
+      },
+      primaryType: "ForwardRequest",
+      domain: {
+        name: req.name,
+        version: req.version,
+        chainId: req.chainId,
+        verifyingContract: req.verifyingContract,
+      },
+      message: {
+        from: req.from,
+        to: req.to,
+        value: req.value,
+        gas: req.gas,
+        nonce: req.nonce,
+        data: req.data,
+        validUntilTime: req.validUntilTime,
+      },
+    },
+  });
+}
+
+/////////////////////////////////////////
+////// call functions ///////////////////
+/////////////////////////////////////////
+
+async function getAccountBalance(address, web3provider) {
+  console.log(`Account: ${address}`);
+
+  // ETH Balance
+  const balance = await web3provider.eth.getBalance(address);
+  console.log(
+    `Balance : ${web3provider.utils.fromWei(balance, "ether")} ${ASSETID}`
+  );
+
+  const web3provider_token = new web3provider.eth.Contract(TOKEN_ABI, TOKEN_CA);
+
+  // token Balance
+  const coinBalance = await web3provider_token.methods
+    .balanceOf(address)
+    .call();
+  const coinName = await web3provider_token.methods.name().call();
+  const coinSymbol = await web3provider_token.methods.symbol().call();
+
+  console.log(
+    `Balance : ${web3provider.utils.fromWei(
+      coinBalance,
+      "ether"
+    )} ${coinSymbol}`
+  );
+}
+
+/////////////////////////////////////////
+////// send functions ///////////////////
+/////////////////////////////////////////
+
+const sendTx = async (_to, _tx, _signer, _gasLimit) => {
+  // check toAddress
+  toAddress = web3_withRelayer.utils.toChecksumAddress(_to);
+  console.log(" toAddress:", toAddress);
+
+  // gasLimit
+  const setGasLimit = _gasLimit;
+  console.log(" setGasLimit:", setGasLimit);
+
+  // gasPrice
+  const gasPrice = await web3_withRelayer.eth.getGasPrice();
+  const gasPriceInGwei = await web3_withRelayer.utils.fromWei(gasPrice, "gwei");
+  console.log(" gasPrice:", gasPrice, "(", gasPriceInGwei, "Gwei)");
+
+  // estimate max Transaction Fee
+  const estimateMaxTxFee = BigInt(setGasLimit) * BigInt(gasPrice);
+  const estimateMaxTxFeeETH = await web3_withRelayer.utils.fromWei(
+    estimateMaxTxFee.toString(),
+    "ether"
+  );
+  console.log(
+    " estimate MAX Tx Fee:",
+    estimateMaxTxFee,
+    "(",
+    estimateMaxTxFeeETH,
+    "ETH)"
+  );
+
+  const createReceipt = await web3_withRelayer.eth
+    .sendTransaction({
+      to: toAddress,
+      from: _signer,
+      data: _tx.encodeABI(),
+      gas: await web3_withRelayer.utils.toHex(setGasLimit),
+    })
+    .once("transactionHash", (txhash) => {
+      console.log(` Send transaction ...`);
+      console.log(` ${EXPLOERE}/tx/${txhash}`);
+    });
+  console.log(
+    ` Tx successful with hash: ${createReceipt.transactionHash} in block ${createReceipt.blockNumber}`
+  );
+
+  return createReceipt;
+};
+
+async function calcDomainSeparator(req) {
+  const domainValue = web3.eth.abi.encodeParameters(
+    ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+    [
+      web3.utils.keccak256(EIP712_DOMAIN_TYPE),
+      web3.utils.keccak256(req.name),
+      web3.utils.keccak256(req.version),
+      req.chainId,
+      req.verifyingContract,
+    ]
+  );
+  return web3.utils.soliditySha3(domainValue);
+}
+
+async function sendTransferByForwarder(req, signature, payerAddr) {
+  const requestTypeHash = web3.utils.soliditySha3(REQUEST_TYPE);
+  const domainSeparator = await calcDomainSeparator(req);
+  const signatureHex = signature.sigHex;
+
+  try {
+    const ForwardRequest = {
+      from: req.from,
+      to: req.to,
+      value: req.value,
+      gas: req.gas,
+      nonce: req.nonce,
+      data: req.data,
+      validUntilTime: req.validUntilTime,
+    };
+
+    const tx = await forwarder_withRelayer.methods.execute(
+      ForwardRequest,
+      domainSeparator,
+      requestTypeHash,
+      req.suffixData,
+      signatureHex
+    );
+
+    console.log("ForwardRequest:", ForwardRequest);
+    console.log("DomainSeparator:", domainSeparator);
+    console.log("RequestTypeHash:", requestTypeHash);
+    console.log("SuffixData:", req.suffixData);
+    console.log("Signature:", signatureHex);
+
+    // パラメータの型もチェック
+    console.log("Types:", {
+      forwardRequest: typeof ForwardRequest,
+      domainSeparator: typeof domainSeparator,
+      requestTypeHash: typeof requestTypeHash,
+      suffixData: typeof req.suffixData,
+      signature: typeof signatureHex,
+    });
+
+    try {
+      const encodeABI = tx.encodeABI();
+      console.log("encodeABI: ", encodeABI);
+    } catch (error) {
+      console.error("encodeABI error:", error);
+      process.exit(1);
+    }
+
+    const receipt = await sendTx(FORWARDER_CA, tx, payerAddr, 2000000);
+    console.log("send permit");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function registerDomainSeparator(name, version, payerAddr) {
+  try {
+    console.log("registerDmainSeparator");
+    const tx = forwarder_withRelayer.methods.registerDomainSeparator(
+      name,
+      version
+    );
+    const receipt = await sendTx(FORWARDER_CA, tx, payerAddr, 2000000);
+    console.log("registerDmainSeparator done!");
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+async function sleepForSeconds(amount) {
+  console.log(`Sleeping for ${amount} seconds...`);
+  await new Promise((r) => setTimeout(r, amount * 1000)); // milliseconds
+  console.log(`${amount} seconds have passed!`);
+}
+
+/////////////////////////////////////////
+////// main functions ///////////////////
+/////////////////////////////////////////
+
+(async () => {
+  console.log("-------- IMPLEMENT ---------");
+  console.log(
+    `forwarder address: ${FORWARDER_CA}, verifying: ${
+      forwarder.options.address !== undefined
+    }`
+  );
+  console.log(
+    `token address: ${TOKEN_CA}, verifying: ${
+      token.options.address !== undefined
+    }`
+  );
+
+  ///////////////////////////////////////////////////////////////////////////////////
+  // STEP1 signer create signature
+  ///////////////////////////////////////////////////////////////////////////////////
+
+  // -------------------FIREBLOCKS VAULT ACCOUNT------------------- //
+  console.log("////////////////////////////");
+  console.log("/////////// STEP1 //////////");
+  console.log("////////////////////////////");
+
+  console.log("========== RELAYER ==========");
+  const vaultAddr2 = await web3_withRelayer.eth.getAccounts();
+  const relayerAddr = vaultAddr2[0];
+  console.log(`relayer address is ${relayerAddr}`);
+  const tc3 = await web3.eth.getTransactionCount(relayerAddr);
+  console.log("transactionCount: ", tc3);
+  console.log("-------- GET VALUE ---------");
+  await getAccountBalance(relayerAddr, web3);
+
+  /*
+    ##############-------------------------------------------
+    # Relayer
+    ##############-------------------------------------------
+*/
+
+  console.log("-------- MINTER --------------------");
+  for (i = 0; i < minters.length; i++) {
+    const vaultName = minters[i].name;
+    const vaultId = minters[i].vaultId;
+    const addr = minters[i].address;
+
+    const tx = token_withRelayer.methods.addMinter(addr);
+    const receipt = await sendTx(TOKEN_CA, tx, relayerAddr, 2000000);
+
+    console.log(
+      `index: ${i}, vaultId: ${vaultId}, name:${vaultName} address: ${addr}, addMinter pushed`
+    );
+    sleepForSeconds(20);
+  }
+})().catch((error) => {
+  console.log(error);
+});
